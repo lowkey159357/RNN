a564401f00bf7e33fda38f838e86f6f8c14e164d
lowkey159357 <40413770+lowkey159357@users.noreply.github.com>
2018-07-17 09:47:06 +0800

    Update model.py

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 
 
 class Model():
-    def __init__(self, learning_rate=0.001,lamda=5e-4,batch_size=16, num_steps=32, num_words=5000, dim_embedding=128, rnn_layers=3):
+    def __init__(self, learning_rate=0.001,lamda=1e-7,batch_size=16, num_steps=32, num_words=5000, dim_embedding=128, rnn_layers=3):
         r"""初始化函数
 
         Parameters
